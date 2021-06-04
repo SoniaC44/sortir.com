eeb3344772c38813dbf72f670d57cd6b1d5b8035
Sonia C <sonia.chavey@gmail.com>
2021-06-04 18:44:19 +0200

    affichage de la page de login si pas user connecté à la racine du site

--- src/Controller/mainController.php
+++ src/Controller/mainController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class mainController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
@@ -13,6 +14,13 @@ class mainController extends \Symfony\Bundle\FrameworkBundle\Controller\Abstract
      * @Route("/", name="main_home")
      */
     public function home(){
-        return $this->render('base.html.twig');
+
+        $user = $this->getUser();
+
+        if($user){
+            return $this->redirectToRoute('sortie_index');
+        }else{
+            return $this->redirectToRoute('app_login');
+        }
     }
 }
