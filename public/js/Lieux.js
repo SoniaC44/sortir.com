08f7267c2f73ec4c5b05840689741cfb8ef5e872
Sonia C <sonia.chavey@gmail.com>
2021-06-08 18:04:09 +0200

    mise en place API + javascript asynchrone pour remplir les lieux en fonction d'une ville

---
+++ public/js/Lieux.js
@@ -0,0 +1,83 @@
+window.addEventListener('DOMContentLoaded', function () { init(); });
+
+function init(){
+    //ajout des evenements sur le change du select ville
+    $selectVille = document.getElementById("sortie_ville");
+    $selectVille.addEventListener("change",() => {
+        initLieux($selectVille.options[$selectVille.selectedIndex].value);
+        initAdresse()
+    });
+
+    //ajout des evenements sur le change du select Lieux
+    $selectLieu = document.getElementById("sortie_lieu");
+    $selectLieu.addEventListener("change",() => {
+        initAdresse();
+    });
+}
+
+function initLieux(id_ville){
+
+    const $selectVille = document.getElementById("sortie_ville");
+    let $selectLieux = document.getElementById('sortie_lieu');
+
+    //si valeur choisie = "choisir une ville"
+    if($selectVille.options[$selectVille.selectedIndex].value !== ""){
+        fetch(url + '/' + id_ville, {'method':'GET'})
+            .then(response=>response.json())
+            .then(response => {
+                $selectLieux = document.getElementById('sortie_lieu');
+                let options="";
+                response.map(lieu => {
+                    options += `<option value="${lieu.id}" data-rue="${lieu.rue}" data-long="${lieu.longitude}" data-lat="${lieu.latitude}" data-codep="${lieu.codePostal}">${lieu.nom}</option>`;
+                });
+                $selectLieux.innerHTML = options;
+
+                //si on a récupéré des lieux ( on a rempli options !)
+                if(options !== ""){
+                    initAdresse();
+                }else{
+                    //si on a pas d'erreur mais pas de lieux pour la ville
+                    //on disabled le select de lieux
+                    $selectLieux.disabled = true;
+
+                    //on efface le contenu des inputs
+                    document.getElementById('sortie_rue').value = "";
+                    document.getElementById('sortie_longitude').value = "";
+                    document.getElementById('sortie_latitude').value = "";
+                    document.getElementById('sortie_codePostal').value = "";
+                }
+
+            })
+            .catch(e=>{
+                alert('Problème lors du chargement des lieux.');
+            });
+
+    }else{
+        $selectLieux.disabled = true;
+        $selectLieux.innerHTML = "";
+    }
+
+}
+
+function initAdresse(){
+
+    const $selectLieu = document.getElementById("sortie_lieu");
+    $selectLieu.disabled = false;
+
+    const $optionSelected = $selectLieu.options[$selectLieu.selectedIndex];
+
+    if($optionSelected.dataset.rue === undefined){
+
+        //undefined dans le cas où on choisit un lieu à partir de la même ville
+        // préremplie par symfony à l'ouverture de la page de modification de sortie
+        let idVille = document.getElementById("sortie_ville").value;
+        initLieux(idVille);
+
+    }else{
+        document.getElementById('sortie_rue').value = $optionSelected.dataset.rue;
+        document.getElementById('sortie_longitude').value = $optionSelected.dataset.long;
+        document.getElementById('sortie_latitude').value = $optionSelected.dataset.lat;
+        document.getElementById('sortie_codePostal').value = $optionSelected.dataset.codep;
+    }
+
+}
--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -10,8 +10,12 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Valid;
 
 class SortieType extends AbstractType
 {
@@ -22,35 +26,112 @@ class SortieType extends AbstractType
             ->add('dateHeureDebut', DateTimeType::class, [
                 'label' => 'Date et heure de la sortie',
                 'html5' => true,
-                'widget' => 'single_text'
+                'widget' => 'single_text',
+                'input_format' => 'Y-m-d H:i',
+                'attr' => ['class' => 'form-control']
+
             ])
             ->add('dateLimiteInscription', DateType::class, [
                 'label' => 'Date limite d\'inscription',
                 'html5' => true,
-                'widget' => 'single_text'
+                'widget' => 'single_text',
+                'input_format' => 'd/m/Y',
+                'attr' => ['class' => 'form-control']
             ])
             ->add('nbInscriptionsMax')
             ->add('duree')
-
-
             ->add('infosSortie')
-
             ->add('ville', EntityType::class, [
                 'mapped' => false,
                 'class' => Ville::class,
                 'choice_label' => 'nom',
-                'placeholder' => 'Choisir une ville'
+                'placeholder' => 'Choisir une ville',
+                'attr' => [
+                    'class' => 'form-select',
+                ]
             ])
-
-            ->add('lieu', EntityType::class, [
-                'class' => Lieu::class,
+            ->add('campus', EntityType::class, [
+                'class' => Campus::class,
                 'choice_label' => 'nom',
-                'placeholder' => 'Choisir un lieu'
+                'attr' => ['class' => 'form-select']
             ])
+            ->add('rue', TextType::class, [
+                'mapped' =>false,
+                'disabled' => true])
+            ->add('codePostal', TextType::class, [
+                'label'=> "Code postal",
+                'mapped' => false,
+                'disabled' => true])
+            ->add('latitude', TextType::class, [
+                'mapped' =>false,
+                'disabled' => true])
+            ->add('longitude', TextType::class, [
+                'mapped' =>false,
+                'disabled' => true])
             ->add('nouveauLieu', LieuType::class, [
-                'mapped' => false
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new Valid()
+                ]
             ])
         ;
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, array($this, 'onPreSetData'));
+        $builder->addEventListener(FormEvents::POST_SET_DATA, [$this, 'onPostSetData']);
+    }
+
+    function onPostSetData(FormEvent $event){
+        $sortie = $event->getData();
+        if($sortie && $sortie->getId() !== null){
+            $form = $event->getForm();
+
+            $lieu = $sortie->getLieu();
+
+            $form->get('rue')->setData($lieu->getRue());
+            $form->get('latitude')->setData($lieu->getLatitude());
+            $form->get('longitude')->setData($lieu->getLongitude());
+            $form->get('codePostal')->setData($lieu->getVille()->getCodePostal());
+
+            $form->get('ville')->setData($lieu->getVille());
+
+        }
+    }
+
+    function onPreSetData(FormEvent $event){
+        $sortie = $event->getData();
+        $form = $event->getForm();
+
+        if ($sortie && $sortie->getId() !== null ) {
+
+            $lieu = $sortie->getLieu();
+            $ville = $lieu->getVille();
+
+            $lieux = $ville->getLieux();
+
+            $form->add('lieu', EntityType::class, [
+                'class' => Lieu::class,
+                'choices' => $lieux,
+                'choice_label' => 'nom',
+                'placeholder' => false,
+                'required' => false,
+                'attr' => ['class' => 'form-select', 'data-rue' => 'test']
+            ]);
+
+            $form->get('lieu')->getConfig()->getOption('choices');
+
+        }else{
+
+            $form->add('lieu', EntityType::class, [
+                'class' => Lieu::class,
+                'choices' => [],
+                'choice_label' => 'nom',
+                'placeholder' => false,
+                'required' => false,
+                'attr' => ['class' => 'form-select']
+            ]);
+
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
